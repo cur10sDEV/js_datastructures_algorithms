b755d8e85024d6cc0aabe2896f6254d561d70185
cur10sDEV <specsx@protonmail.com>
2023-01-05 11:51:21 +0530

    linear search algo added

---
+++ searching/linearSearch.js
@@ -0,0 +1,14 @@
+function linearSearch(arr, value) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === value) return i;
+	}
+	return -1;
+}
+
+const index = linearSearch([56, 53, 88, 22, 14, 6, 3, 99, 57, 45], 3);
+console.log(index);
+
+// Big O
+// Best Case O(1)
+// Average Case O(n)
+// Worst Case O(n)
