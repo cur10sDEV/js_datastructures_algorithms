b4eb1081304c8b850c5bb2d22c2a76321063b1bf
cur10sDEV <specsx@protonmail.com>
2023-01-05 13:27:46 +0530

    recursive binary search added

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -1,6 +1,4 @@
-function binarySearch(arr, val) {
-	let left = 0;
-	let right = arr.length - 1;
+function binarySearch(arr, val, left, right) {
 	let mid = Math.floor((left + right) / 2);
 
 	while (left <= right) {
@@ -12,9 +10,21 @@ function binarySearch(arr, val) {
 	return -1;
 }
 
+// recursive version
+function binarySearch(arr, val, left, right) {
+	let mid = Math.floor((left + right) / 2);
+	if (left <= right) {
+		if (val === arr[mid]) return mid;
+		if (val < arr[mid]) return binarySearch(arr, val, left, mid - 1);
+		if (val > arr[mid]) return binarySearch(arr, val, mid + 1, right);
+	}
+	return -1;
+}
+
+const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21];
+console.log(binarySearch(arr, 21, 0, arr.length - 1));
+
 // Big O
 // Best Case - O(1)
 // Average Case - O(log n)
 // Worst Case - O(log n)
-
-console.log(binarySearch([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21], 11));
