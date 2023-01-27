778ecd7d950d38c9ac748157f57ca57addd4a6d4
cur10sDEV <specsx@protonmail.com>
2023-01-27 10:19:23 +0530

    insertion Sort added

---
+++ sorting/insertionSort.js
@@ -0,0 +1,27 @@
+const arr = [24, 76, 14, 9, 65, -3];
+
+function insertionSort(arr) {
+	const swap = (arr, i, j) => {
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	};
+	for (let i = 1; i < arr.length; i++) {
+		let j = i;
+		if (arr[j] < arr[j - 1]) {
+			while (arr[j] < arr[j - 1]) {
+				swap(arr, j, j - 1);
+				j--;
+			}
+		}
+	}
+	return arr;
+}
+
+console.log(insertionSort(arr));
+
+// Big O
+// Best Case - O(n)
+// Average Case - O(n**2)
+// Worst Case - O(n**2)
+
+// note
+// insertion sort works very well if you need your data to be continously sorted that is new elements are being added to maintain a running sort
